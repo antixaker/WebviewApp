ab7d5cdc17876a538a0fc62ed028423db7757507
Alex <proskuraaleksandr93@gmail.com>
2016-05-25 00:42:48 +0300

    Create logic for redirections from main menu to lessons page

--- OpenGLGuide/OpenGLGuide/ViewModels/StartedViewModel.cs
+++ OpenGLGuide/OpenGLGuide/ViewModels/StartedViewModel.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Windows.Input;
-using Xamarin.Forms;
+using System.Collections.ObjectModel;
 using FreshMvvm;
 using PropertyChanged;
-using System.Collections.ObjectModel;
 
 namespace OpenGLGuide.ViewModels
 {
@@ -18,19 +16,29 @@ namespace OpenGLGuide.ViewModels
         public StartedViewModel()
         {
             MenuItems = new ObservableCollection<MenuItemViewModel>() { 
-                new MenuItemViewModel("Уроки по OpenGL", null, OpenGLGuide.Enums.MenuItemTypes.GroupItem),
-                new MenuItemViewModel("Лабораторные работы", null, OpenGLGuide.Enums.MenuItemTypes.GroupItem) 
+                new MenuItemViewModel("Уроки по OpenGL", null, null, OpenGLGuide.Enums.MenuItemTypes.GroupItem),
+                new MenuItemViewModel("Лабораторные работы", null, null, OpenGLGuide.Enums.MenuItemTypes.GroupItem) 
             };
 
-            _lazyLessonsSubItems = new Lazy<List<MenuItemViewModel>>(
-                () => new List<MenuItemViewModel>() {
-                    new MenuItemViewModel("Урок 1", "nehe01.jpg")
-                });
+            _lazyLessonsSubItems = new Lazy<List<MenuItemViewModel>>(() =>
+                {
+                    var list = new List<MenuItemViewModel>();
+                    for (int i = 1; i <= 42; i++)
+                        list.Add(new MenuItemViewModel(string.Format("Урок {0}", i), string.Format("NeheImages/nehe{0}.jpg", IntToStringConverter(i)), string.Format("nehe{0}.htm", IntToStringConverter(i))));
+
+                    return list;
+                }
+            );
+        }
+
+        private string IntToStringConverter(int value)
+        {
+            return value < 10 ? string.Format("0{0}", value) : value.ToString();
         }
 
         public ObservableCollection<MenuItemViewModel> MenuItems { get; set; }
 
-        public void OpenPage(object item)
+        public async void OpenPage(object item)
         {
             var itemViewModel = item as MenuItemViewModel;
             if (itemViewModel == null)
@@ -40,12 +48,35 @@ namespace OpenGLGuide.ViewModels
             {
                 if (itemViewModel.ItemText == "Уроки по OpenGL")
                 {
-                    var startindex = 0;
-                    foreach (var itemValue in _lazyLessonsSubItems.Value)
+                    if (!itemViewModel.IsDropDownOpened)
                     {
-                        MenuItems.Insert(++startindex, itemValue);
+                        var startindex = 0;
+                        foreach (var itemValue in _lazyLessonsSubItems.Value)
+                        {
+                            MenuItems.Insert(++startindex, itemValue);
+                        }
+                        itemViewModel.IsDropDownOpened = true;
+                    }
+                    else
+                    {
+                        var tmp = new MenuItemViewModel[MenuItems.Count];
+                        MenuItems.CopyTo(tmp, 0);
+                        var tmpList = new List<MenuItemViewModel>(tmp);
+
+                        foreach (var itemValue in _lazyLessonsSubItems.Value)
+                        {
+                            if (tmpList.Contains(itemValue))
+                                tmpList.Remove(itemValue);
+                        }
+
+                        MenuItems = new ObservableCollection<MenuItemViewModel>(tmpList);
+                        itemViewModel.IsDropDownOpened = false;
                     }
                 }
+            }
+            else if (itemViewModel.ItemType == OpenGLGuide.Enums.MenuItemTypes.DefaultItem)
+            {
+                await CoreMethods.PushPageModel<WebViewModel>(itemViewModel.PageHtmlFileName);
             }
         }
 
